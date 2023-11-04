3501359617456a70e0ea31ededb2f493fd9ffb28
imran <muhammadalimran22@gmail.com>
2023-11-05 03:37:15 +0700

    initial dev

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const { PORT = 3000 } = process.env;
 app.use(express.json());
 app.use("/api/v1", require("./routes/postImage.routes"));
 
+app.get("/", (req, res) => {
+  return res.json({
+    status: true,
+    message: "welcome to deployment with railway.app",
+    err: null,
+    data: null,
+  });
+});
+
 app.use((req, res, next) => {
   res.status(404).json({
     status: false,
